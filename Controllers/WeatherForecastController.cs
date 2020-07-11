d1ef4a749c1f6fcbf796910a205c4a3ff7a9ddc3
Bajinder Singh <bajinder2017@gmail.com>
2020-07-11 21:18:10 +0100

    Thand

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace HelloAspNetCore.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Bobby"
+            "Thand", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Bobby"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
