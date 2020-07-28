d7b5e11e4b9638a6d89157c685d5a28a81fa9c4e
Bajinder Singh <bajinder2017@gmail.com>
2020-07-28 13:46:56 +0100

    test2

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace HelloAspNetCore.Controllers
 {
@@ -11,29 +12,26 @@ namespace HelloAspNetCore.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+
         private static readonly string[] Summaries = new[]
         {
             "testing", "Bracing", "Sydney", "fool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Bobby"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IOptions<MyConfig> config;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IOptions<MyConfig> config)
         {
             _logger = logger;
+            this.config = config;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public MyConfig Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return config.Value;
         }
     }
 }
